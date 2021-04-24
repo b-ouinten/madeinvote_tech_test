877ab76d74eee4e1673fadf4a0a49dc657ee6d3d
ouin85 <bouinten@yahoo.fr>
2021-04-24 09:43:28 +0200

    correct some spelling mistakes

--- lib/application.rb
+++ lib/application.rb
@@ -1,30 +1,30 @@
 class Application
   def perform
-    puts '-' * 126
-    puts "|#{' ' *3}Hello ! Are you fine ! This program can calculate, with two approaches, the greatest binary gap of a positive integer.#{' ' *3}|"
-    puts '-' * 126
+    puts '-' * 134
+    puts "|#{' ' * 3}Hello ! Are you fine ! This program can calculate, with two approachs, the greatest binary gap of a strictly positive integer.#{' ' * 3}|"
+    puts '-' * 134
     
     menu = Menu.new
     
     while (true)
-      puts "|#{' ' * (126 - 1 - 1)}|"
+      puts "|#{' ' * (134 - 1 - 1)}|"
       
       positive_integer = PositiveInteger.new
       
-      menu.ask_for_choose_approache
-      choose = menu.entries[:choose]
-      break if choose.eql? '3'
+      menu.ask_choose_approach
+      choice = menu.entries[:choice]
+      break if choice.eql? '3'
       
       loop do
-        menu.ask_for_integer_value
+        menu.ask_integer_value
         break if positive_integer.update_value(menu.entries[:integer])
       end
       integer_value = positive_integer.value
-      greatest_binary_gap = choose == '1' ? positive_integer.find_greatest_binary_gap : positive_integer.find_greatest_binary_gap_elegant_way
+      greatest_binary_gap = choice == '1' ? positive_integer.find_greatest_binary_gap : positive_integer.find_greatest_binary_gap_elegant_way
       puts  "|The greatest binary gap of #{integer_value} is #{greatest_binary_gap}."\
-            "#{' ' * (126 - 27 - integer_value.to_s.size - greatest_binary_gap.to_s.size - 6 - 1)}|"
+            "#{' ' * (134 - 27 - integer_value.to_s.size - greatest_binary_gap.to_s.size - 6 - 1)}|"
     end
     
-    puts "|Bye !#{' ' * (126 - 6 - 1)}|"
+    puts "|Bye !#{' ' * (134 - 6 - 1)}|"
   end
 end
--- lib/menu.rb
+++ lib/menu.rb
@@ -5,26 +5,26 @@ class Menu
     @entries = {}
   end
   
-  def ask_for_integer_value
-    puts "|Enter a strictly positive integer !#{' ' * (126 - 36 -1)}|"    
+  def ask_integer_value
+    puts "|Enter a strictly positive integer !#{' ' * (134 - 36 -1)}|"    
     print '|'
     input = gets.chomp
-    puts "#{' ' *(126 - 1)}|"
+    puts "#{' ' *(134 - 1)}|"
     @entries[:integer] = input.to_i
   end
     
-  def ask_for_choose_approache
+  def ask_choose_approach
     input = ''
-    puts "|1. First approache, see the PositiveInteger#find_greatest_binary_gap method.#{' ' * (126 - 77 - 1)}|"
-    puts "|2. Second approache, see the PositiveInteger#find_greatest_binary_gap_elegant_way method.#{' ' * (126 - 90 - 1)}|"
-    puts "|3. Quit.#{' ' * (126 - 9 - 1)}|"
+    puts "|1. First approach, see the PositiveInteger#find_greatest_binary_gap method.#{' ' * (134 - 76 - 1)}|"
+    puts "|2. Second approach, see the PositiveInteger#find_greatest_binary_gap_elegant_way method.#{' ' * (134 - 89 - 1)}|"
+    puts "|3. Quit.#{' ' * (134 - 9 - 1)}|"
     loop do
       print '|'
       input = gets.chomp
-      puts "#{' ' *(126 - 1)}|"
+      puts "#{' ' *(134 - 1)}|"
       break if (input.eql?('1') || input.eql?('2') || input.eql?('3'))
-      puts "|You must enter only 1, 2 or 3 ! Try again please !#{' ' * (126 - 51 - 1)}|"
+      puts "|You must choose between 1, 2 or 3 ! Try again please !#{' ' * (134 - 55 - 1)}|"
     end
-    @entries[:choose] = input
+    @entries[:choice] = input
   end
 end
