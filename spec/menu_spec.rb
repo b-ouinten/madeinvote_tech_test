8a6af479bfca6020edb6f34de79bbbf46af9d9df
ouin85 <bouinten@yahoo.fr>
2021-04-25 03:04:19 +0200

    correct some spelling mistakes

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,7 +6,7 @@ describe Menu do
   end
   
   describe "ask_integer_value" do
-    it "should update entries[:integer] attribut by user input" do
+    it "should update entries[:integer] attribute by user input" do
       allow($stdin).to receive(:gets).and_return '9'
       @menu.ask_for_integer_value
       expect(@menu.entries[:integer]).to eq 9
@@ -14,13 +14,13 @@ describe Menu do
   end
   
   describe "ask_for_choose_approach" do
-    it "should update entries[:choice] attribut by user input" do
+    it "should update entries[:choice] attribute by user input" do
       allow($stdin).to receive(:gets).and_return '1'
       @menu.ask_for_choose_approach
       expect(@menu.entries[:choice]).to eq '1'
     end
 
-    it "should ask again when user input is invalid and update entries[:choice] attribut with a next valid user input" do
+    it "should ask again when user input is invalid and update entries[:choice] attribute with a next valid user input" do
       allow($stdin).to receive(:gets).and_return '4', '2'
       expect{ @menu.ask_for_choose_approach }.to output(
         "|1. First approach, see the PositiveInteger#find_greatest_binary_gap method.#{' ' * (134 - 76 - 1)}|\n" \
